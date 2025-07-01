e6fbb6bdf1f0b654af9f806e922fd3799107c843
unknown <smroady@asu.edu>
2025-07-01 11:31:42 -0400

    Refactor FileHandle
    Moved reusable code to function outside of main, begin implementing file parsing.

--- FileHandle.py
+++ FileHandle.py
@@ -22,6 +22,46 @@ def splitValues(contents):
     line = contents.split("\t")
     return line
 
+def fileExcluder(metaDataContent):
+    try:
+        filterdata = ""
+        valuepairs = {}
+        while filterdata != 113:
+            print("Attributes are: " + str(metaDataContent[0]))
+            filterdata = input("Enter an attribute you would like to filter for or q to exit: "
+                               "Enter a value between 0 and " + str(metaDataContent[0].__len__() - 1) + ": ")
+            temp = ord(filterdata)
+            if temp == 113:
+                break
+            else:
+                colAtt = int(filterdata)
+            print("Filtering for : " + metaDataContent[0][colAtt])
+            print("Enter items you would like to exclude, enter q when done: ")
+            inclusionArray = []
+            item = ""
+            while item != "q":
+                item = input("Enter a value or q: ")
+                if item != "q":
+                    inclusionArray.append(item)
+                print(item + " entered")
+            valuepairs[colAtt] = inclusionArray
+            inclusionArray = []
+        return valuepairs
+    except Exception as e:
+        print(f"Error: '{e}'")
+
+def parseFile(valuePair, file):
+    newFile = []
+    flag = 0
+    for i in file:
+        for key in valuePair:
+            if file[i][key] in valuePair[key]:
+                flag = 1
+        if flag != 1:
+            newFile.append(file[i])
+        flag = 0
+    return newFile
+
 if __name__ == "__main__":
     if len(sys.argv) == 3:
         # Format is: scriptname metadata1.txt data1.txt
@@ -30,31 +70,14 @@ if __name__ == "__main__":
         directory = os.getcwd() + "\\input" + "\\"
         metaDataContent = take_file_input(directory + metadata)
         dataContent = take_file_input(directory + data)
-        print(metaDataContent)
-        print()
-        print(dataContent)
-        try:
-            filterData = ""
-            while filterData != 113:
-                print("Let's start with the meta data file!")
-                print("Attributes are: " + str(metaDataContent[0]))
-                filterData = input("Enter an attribute you would like to filter for or q to exit: "
-                                   "Enter a value between 0 and " + str(metaDataContent[0].__len__() - 1) + ": ")
-                temp = ord(filterData)
-                if temp == 113:
-                    break
-                else:
-                    colAtt = int(filterData)
-                print("Filtering for : " + metaDataContent[0][colAtt])
-                print("Enter items you would like to exclude, enter q when done: ")
-                inclusionArray = []
-                item = ""
-                while item != "q":
-                    item = input("Enter a value or q: ")
-                    inclusionArray.append(item)
-                    print(item + " entered")
-        except Exception as e:
-            print(f"Error: '{e}'")
+
+        metaDataValue = fileExcluder(metaDataContent)
+        dataValue = fileExcluder(dataContent)
+
+        filteredMetaData = parseFile(metaDataValue, metaDataValue[1:])
+        filteredData = parseFile(dataContent, dataContent[1:])
+
+
 
     else :
         print(os.getcwd())
